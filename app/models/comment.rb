333540927b4b2d5e4804f05f0145aa968f183047
Luciana Sugeta <lulusugeta@gmail.com>
2021-07-07 18:26:51 -0400

    commit tree cefd1d535ee59571ce4988933ea8429cdf0e8f25

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,16 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user
+
+  def user_attributes=(user_attributes)
+    # user_attributes.values.each do |user_attribute|
+    #   if user_attribute["username"].blank?
+    #     user = User.find_or_create_by(user_attribute)
+    #     self.users << user
+    #   end
+    # end
+    self.user = User.find_or_create_by(username: user_attributes[:username]) unless user_attributes[:username].blank?
+  end
 
 end
